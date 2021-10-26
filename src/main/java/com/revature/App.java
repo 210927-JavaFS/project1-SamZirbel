094356491955a04288bb3a7e4b8b01d2350b736e
Sam Zirbel <samuelzirbel@gmail.com>
2021-10-25 22:06:53 -0700

    Created App Main Java File

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,37 @@
+package com.revature;
+
+import com.revature.controllers.Controller;
+import com.revature.controllers.EmployeeController;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+public class App {
+	
+	public static Javalin app;
+	
+	public static void main(String[] args) {
+		
+		app = Javalin.create((config) -> {
+			
+			config.addStaticFiles("/Static", Location.CLASSPATH);
+			
+		});
+				
+		configure(new EmployeeController());
+		
+		app.start(8081);
+		
+	}
+	
+	private static void configure(Controller ... controllers) {
+		
+		for (Controller activecontroller : controllers) {
+			
+			activecontroller.addRoutes(app);
+			
+		}
+		
+	}
+
+}
